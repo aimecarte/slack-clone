7e55d522a330fb64ba178f1b605588a9c969ceee
agamecarte <aga.mecarte@outlook.com>
2025-02-10 15:58:51 +0800

    add a login page form

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,3 +1,3 @@
 export default function useLogin() {
-    return {token: true}
+    return {token: false}
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,15 @@
 import { Navigate } from "react-router-dom";
 import useLogin from "../hooks/useLogin"
+import { LoginForm } from "../components/login-form";
 
 export default function Login() {
     const auth = useLogin();
 
     return (
         !auth.token ? (
-            <h1>Login Page</h1>
+            <div className="max-w-sm absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <LoginForm />
+            </div>
         ) : (
             <Navigate to='/' />
         )
